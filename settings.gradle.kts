f76935e96a593b512c89a411eff94d2073c9f979
Ryan Dens <ryandens735@gmail.com>
2025-06-21 16:37:40 -0400

    :pencil2: rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "quarkus-component-test-reproducer"
+rootProject.name = "quarkus-s3-devservices-reproducer"
 include("lib")
