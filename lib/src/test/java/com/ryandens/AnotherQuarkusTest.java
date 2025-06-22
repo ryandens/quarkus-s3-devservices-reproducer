72fade9fab0a202cea56e28b45531bd7e559e836
Ryan Dens <ryandens735@gmail.com>
2025-06-22 11:09:25 -0400

    :alembic:

--- lib/src/test/java/com/ryandens/AnotherQuarkusTest.java
+++ lib/src/test/java/com/ryandens/AnotherQuarkusTest.java
@@ -3,11 +3,13 @@ package com.ryandens;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @QuarkusTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AnotherQuarkusTest {
 
   @Inject S3Client s3Client;
